b45b513f211b3c490973e1daf4c107efc6048a30
Prashansa Meyn <pm@globalpay.dk>
2024-07-22 23:13:52 +0200

    SendGrid first

---
+++ EmailSwitch/Services/SendGrid/SendGridService.cs
@@ -0,0 +1,20 @@
+﻿using EmailSwitch.Common;
+using EmailSwitch.Common.DTOs;
+using HumanLanguages;
+using SMSwitch.Common.DTOs;
+
+namespace EmailSwitch.Services.SendGrid
+{
+	public sealed class SendGridService : IServiceEmails
+	{
+		public Task<EmailSwitchResponseSendOTP> SendOTP(EmailIdentifier email, MobileNumber[] verifiedMobileNumbers, EmailIdentifier[] verifiedEmails, HashSet<LanguageIsoCode> preferredLanguageIsoCodeList, UserAgent userAgent)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<EmailSwitchhResponseVerifyOTP> VerifyOTP(EmailIdentifier email, string OTP)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
